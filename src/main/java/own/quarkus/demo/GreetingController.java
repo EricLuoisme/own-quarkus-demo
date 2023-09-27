4115d34276a0e1acb783ee9c3596953dd315afc2
roylic <roylic@wokoworks.com>
2023-09-27 17:53:11 +0800

    Quarkus' @Inject can replace @Autowired in some situations

--- src/main/java/own/quarkus/demo/GreetingController.java
+++ src/main/java/own/quarkus/demo/GreetingController.java
@@ -16,6 +16,7 @@
 
 package own.quarkus.demo;
 
+import jakarta.inject.Inject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +26,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    private final GreetingBean greetingBean;
+    // can use @Inject to replace @Autowired in specific situation
+    @Inject
+    private GreetingBean greetingBean;
 
-    public GreetingController(GreetingBean greetingBean) {
-        this.greetingBean = greetingBean;
-    }
+//    public GreetingController(GreetingBean greetingBean) {
+//        this.greetingBean = greetingBean;
+//    }
 
     @GetMapping("/{name}")
     public Greeting hello(@PathVariable(name = "name") String name) {
